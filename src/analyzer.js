460ade450be7d2071fa0140454de028b704ac771
MarcelaGaravito <mache095@gmail.com>
2023-08-29 14:51:25 -0600

    Actualizacion 29 agosto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,48 @@
+//TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(/\s+/);
+    return words.length; 
   },
+
+//TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
+
+//TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const textWithoutPunctuation = text.replace(/[^\w\s]/g, '');
+    const textWithoutSpaces = textWithoutPunctuation.replace(/\s/g, '');
+      return textWithoutSpaces.length;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+
+//TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+getAverageWordLength: (text) => {  
+  const words = text.split(/\s+/); //divide texto e palabras 
+  let totalLength= words.join('').length; // se uctiliza para mantener la suma total de las longitudes 
+  //for (let i= 0; // declara una variable i = 0 para recorrer las posiciones del array 'word'
+   // i <words.length; i++) {  // mientras i sea menor quel array esta se seguira ejecutando
+    //totalLength += words[i].length; 
+    //console.log (totalLength);
+  //}
+  //hacer la division 
+    return totalLength/words.length;
+},
+//TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\d+/g);
+    return numbers ? numbers.length : 0;
+  
   },
+
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+      let numbers = text.match(/\d+/g);
+      if (!numbers) return 0;
+      const sum = numbers.reduce((total, number) => total + parseInt(number, 10), 0);
+      return sum;
   },
-};
+}
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import analyzer from './analyzer.js';
-
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// Dom references:
+const userInput = document.querySelector('textarea');
+const wordCount = document.querySelector('[data-testid="word-count"]'); 
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+const tre = document.querySelector('[data-testid="average-word-lenght"]');
+console.log (tre,"prueba1");
+const resetButton = document.getElementById("reset-button");
+
+// al levantar la tecla me da los resultados
+
+userInput.addEventListener('keyup', () => {
+  const text = userInput.value; 
+  const iCountC=analyzer.getWordCount(text);
+  wordCount.innerHTML = "Total palabras:"+iCountC;
+});
+userInput.addEventListener('keyup', () => {
+  const text = userInput.value; 
+  const iCountC=analyzer.getCharacterCount(text);
+  characterCount.innerHTML = "Total caracteres:"+iCountC;
+});
+userInput.addEventListener('keyup', () => {
+  const text = userInput.value; 
+  const iCountC=analyzer.getCharacterCountExcludingSpaces(text);
+  characterNoSpacesCount.innerHTML = "Caracteres excluyendo espacios:"+iCountC;
+});
+userInput.addEventListener('keyup', () => {
+  const text = userInput.value; 
+  const iCountC=analyzer.getAverageWordLength(text);
+  console.log (tre.textContent,"prueba");
+  tre.innerHTML = "Longitud de palabras:"+iCountC;
+  
+});
+userInput.addEventListener('keyup', () => {
+  const text = userInput.value; 
+  const iCountC=analyzer.getNumberCount(text);
+  numberCount.innerHTML = "Total números:"+iCountC;
+});
+userInput.addEventListener('keyup', () => {
+  const text = userInput.value; // input
+  const iCountC=analyzer.getNumberSum(text);
+  numberSum.innerHTML = "Suma números:"+iCountC;
+});
+
+
+ 
+
+// boton de limpiar, reestablece el contenido de res
+resetButton.addEventListener('click', () => {
+  userInput.value = '';
+
+});
